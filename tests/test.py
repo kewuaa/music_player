845ac635d26d2a36782a3a70e7ba4b4b8101de8b
kewuaa <1692525710@qq.com>
2022-09-20 14:08:42 +0800

    update

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,6 @@
-import sys
-import asyncio
-sys.path.append('.')
+# import sys
+# sys.path.append('.')
 import pymusic
 
 
 pymusic.run()
-loop = asyncio.get_event_loop()
-# _kg = kg.KgSource(loop=loop)
-# info = loop.run_until_complete(_kg._get_info('刚刚好'))
-# print(info)
-# url = loop.run_until_complete(_kg._get_source(info[0].id))
-# print(url)
--- tests/urltest.py
+++
@@ -1,9 +0,0 @@
-from urllib import request
-from re import compile
-
-from lxml.html import fromstring
-
-url = 'https://music.91q.com/search?word=%E5%88%9A%E5%88%9A%E5%A5%BD'
-res = request.urlopen(url)
-text = res.read().decode('gbk')
-print(text)
